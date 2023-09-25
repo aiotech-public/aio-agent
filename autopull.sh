d8e2e0acc8ced8b58d7d0339fcecdc9fb3c2c8db
NK <n@sry.systems>
2023-09-25 21:33:04 +0300

    -

--- autopull.sh
+++ autopull.sh
@@ -6,6 +6,7 @@ local_commit_hash=$(git rev-parse --short HEAD)
 
 if [[ $pull_result != "QWxyZWFkeSB1cCB0byBkYXRlLgo=" ]]; then
     echo "NEW COMMIT $local_commit_hash `date`" >> /var/log/git.log
+    docker-compose pull
     docker-compose restart
     echo "APP RESTARTED `date`" >> /var/log/git.log
 fi
