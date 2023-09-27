c28963d209901beb44c33ef8f8799280eadaa481
aiotech-public <144063030+aiotech-public@users.noreply.github.com>
2023-09-27 10:59:39 +0300

    Update autopull.sh

--- autopull.sh
+++ autopull.sh
@@ -6,11 +6,8 @@ local_commit_hash=$(git rev-parse --short HEAD)
 
 if [[ $pull_result != "QWxyZWFkeSB1cCB0byBkYXRlLgo=" ]]; then
     echo "NEW COMMIT $local_commit_hash `date`" >> /var/log/git.log
+    docker-compose pull
     docker-compose up --detach
     echo "APP RESTARTED `date`" >> /var/log/git.log
 fi
 
-if docker pull prodaiotech/agentgo:latest | grep -q "Downloaded newer image"; then
-    docker-compose up --detach
-    echo "APP RESTARTED `date`" >> /var/log/git.log
-fi
